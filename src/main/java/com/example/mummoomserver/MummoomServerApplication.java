d9ddda543d333fb010cbc3c847da6cbd8fac661b
jw0111 <59572441+jw0111@users.noreply.github.com>
2022-01-12 14:57:28 +0900

    init
    init

---
+++ src/main/java/com/example/mummoomserver/MummoomServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mummoomserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MummoomServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MummoomServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mummoomserver/MummoomServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mummoomserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MummoomServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
