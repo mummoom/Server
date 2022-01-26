daadd8280e43841f3880d77af7ae0b090597eb5c
hyojeongyun <ynhyojng@gmail.com>
2022-01-26 14:30:14 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,4 +34,5 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  -Dspring.config.location=classpath:/application.yml, /home/ec2-user/app/application-real-db.yml \
+  $REPOSITORY/$JAR_NAME 2>&1 &
