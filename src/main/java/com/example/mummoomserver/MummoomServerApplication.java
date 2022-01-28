1c28edbb69344cd84503780e458ad9dc1d6ec368
ndb796 <joje1003@naver.com>
2022-01-28 12:44:48 +0900

    login 1st correct

--- src/main/java/com/example/mummoomserver/MummoomServerApplication.java
+++ src/main/java/com/example/mummoomserver/MummoomServerApplication.java
@@ -1,19 +1,11 @@
 package com.example.mummoomserver;
 
-import com.example.mummoomserver.oauthlogin.OauthLoginApplication;
 import com.example.mummoomserver.oauthlogin.config.properties.AppProperties;
 import com.example.mummoomserver.oauthlogin.config.properties.CorsProperties;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableJpaAuditing
