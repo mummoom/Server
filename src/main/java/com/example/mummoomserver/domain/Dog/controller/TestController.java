9ecf0b6ec2ec96d869f6fa55ceb75b84836626ab
hyojeongyun <ynhyojng@gmail.com>
2022-01-31 06:28:45 +0900

    appspec

--- src/main/java/com/example/mummoomserver/domain/Dog/controller/TestController.java
+++ src/main/java/com/example/mummoomserver/domain/Dog/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/testteTstteste")
+    @GetMapping("/secondtestdferfwefwf")
     public String test(){
         return "test";
     }
