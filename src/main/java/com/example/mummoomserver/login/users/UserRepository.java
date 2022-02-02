36b541daf12566e88f40374e519c60ae22bfda65
jw0111 <59572441+jw0111@users.noreply.github.com>
2022-02-02 23:07:14 +0900

    커뮤니티인데 오류있음

--- src/main/java/com/example/mummoomserver/login/users/UserRepository.java
+++ src/main/java/com/example/mummoomserver/login/users/UserRepository.java
@@ -2,13 +2,15 @@ package com.example.mummoomserver.login.users;
 
 import com.example.mummoomserver.login.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 // 검사하는 메소드 저장
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByUserIdx(Long userIdx);
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
     boolean existsByEmail(String email);
