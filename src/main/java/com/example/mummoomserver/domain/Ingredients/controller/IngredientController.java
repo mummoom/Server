b64fe26a4885e586ce039242431cf3e95ad589ee
philos1234 <48795102+philos1234@users.noreply.github.com>
2022-02-05 20:27:21 +0900

    Ingredient swagger 수정

--- src/main/java/com/example/mummoomserver/domain/Ingredients/controller/IngredientController.java
+++ src/main/java/com/example/mummoomserver/domain/Ingredients/controller/IngredientController.java
@@ -6,9 +6,7 @@ import com.example.mummoomserver.config.resTemplate.ResponseTemplateStatus;
 import com.example.mummoomserver.domain.Ingredients.Category;
 import com.example.mummoomserver.domain.Ingredients.entity.Ingredients;
 import com.example.mummoomserver.domain.Ingredients.service.IngredientListService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +30,10 @@ public class IngredientController {
             , notes = "카테고리별 재료를 표시해준다  0 = 채소 , 1 = 생선 , 2 = 고기, 3 = 유제품 , 4 = 어패류 ,5 = 곡류, 6 = 과일 , 7 = 기타 ")
     @GetMapping("/api/ingredients/{categoryNum}")
     @ApiImplicitParam(name ="categoryNum", value="카테고리 번호, int 값", required = true)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "카테고리 재료 요청 성공 "),
+            @ApiResponse(code = 4000, message = "잘못된 파라미터 값입니다 0~7까지의 파라미터만 받을 수 있습니다")
+    })
     public ResponseTemplate<List<Ingredients>> getIngredientList(@PathVariable(name ="categoryNum") int categoryNum){
 
         try {
@@ -54,12 +56,16 @@ public class IngredientController {
             , notes = "도형 표시 로 마크된 간단히 알아보는 음식 정보 리스트를 불러온다  0 = 엑스표시, 1 = 세모표시 , 2 = 동그라미 표시  ")
     @GetMapping("/api/simple/ingredients/{level}")
     @ApiImplicitParam(name ="level", value="0 ,1 , 2 중 하나의 값이 level이 된다", required = true)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "요청 성공 "),
+            @ApiResponse(code = 4000, message = "잘못된 파라미터 값입니다 0~2까지의 파라미터만 받을 수 있습니다")
+    })
     public ResponseTemplate<List<Ingredients>> getSimpleIngredientX(@PathVariable(name="level") int level) throws ResponeException {
 
         if( 0<= level &&level <=2 )  return new ResponseTemplate<>(ingredientListService.simpleList(level));
         else{   //그 외는 레벨이 안됨
             log.info("level 에러!");
-            throw  new ResponeException(ResponseTemplateStatus.FAIL);
+            throw  new ResponeException(ResponseTemplateStatus.INVALID_PARAM);
         }
 
 
