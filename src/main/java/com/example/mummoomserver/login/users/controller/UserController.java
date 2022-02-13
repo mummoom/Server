a75c1047f6c0aa2d3d0e272a0191d3743b0999f9
ndb796 <joje1003@naver.com>
2022-02-14 00:30:33 +0900

    소셜로그인 중복검사 및 랜덤 이메일 닉네임 설정

--- src/main/java/com/example/mummoomserver/login/users/controller/UserController.java
+++ src/main/java/com/example/mummoomserver/login/users/controller/UserController.java
@@ -47,6 +47,7 @@ import java.io.IOException;
 import java.util.Date;
 import java.lang.reflect.Member;
 import java.util.Optional;
+import java.util.Random;
 
 @Slf4j
 @RestController
@@ -188,8 +189,18 @@ public class UserController {
             } else {
                 //DB에 정보가 없다면 DB에 유저정보 저장
                 log.info("DB에 유저 정보 없음");
-                UserDto user = googleUser.toUserDto(googleUser.getEmail(),
-                        googleUser.getName(),
+                String googleEmail = googleUser.getEmail();
+                String googleNickName = googleUser.getName();
+                Random rnd = new Random();
+                String randomStr = String.valueOf((char) ((int) (rnd.nextInt(26)) + 65));
+                if(userRepository.existsByEmail(googleEmail)==false) {
+                    googleEmail = googleEmail + randomStr; // 어떤 랜덤한 값 부여해주기
+                }
+                if(userRepository.existsByNickName(googleNickName)==true) {
+                    googleNickName = googleNickName + randomStr; // 어떤 랜덤한 값 부여해줄 지 생각하기
+                }
+                UserDto user = googleUser.toUserDto(googleEmail,
+                        googleNickName,
                         googleUser.getPicture());
                 userService.saveOAuthUser(user);
                 dog_exist = false;
@@ -265,6 +276,7 @@ public class UserController {
         if (updateImgRequest.getImgUrl() == null) //변경하는 이미지가 없는 경우
             return new ResponseTemplate<>(ResponseTemplateStatus.EMPTY_UPDATE);
         user.updateImgUrl(updateImgRequest.getImgUrl());
+        userRepository.save(user);
         String result = "프로필 이미지 수정에 성공했습니다.";
         return new ResponseTemplate<>(result);
     }
@@ -290,8 +302,8 @@ public class UserController {
             return new ResponseTemplate<>(ResponseTemplateStatus.NICKNAME_DUPLICATED);}
         //유효성 검사
         if (bindingResult.hasErrors()) return new ResponseTemplate<>(ResponseTemplateStatus.INCORRECT_NICKNAME);
-
         user.updateNickName(updateNickNameRequest.getNickName());
+        userRepository.save(user);
         String result = "닉네임 수정에 성공했습니다.";
         return new ResponseTemplate<>(result);
     }
@@ -312,16 +324,14 @@ public class UserController {
         if (updatePwdRequest.getNewPassword() == null) return new ResponseTemplate<>(ResponseTemplateStatus.EMPTY_UPDATE_PASSWORD);
         try {
             String email = userService.getAuthUserEmail();
-            User member = userRepository.findByEmail(email).get();
+            User user = userRepository.findByEmail(email).get();
             // 해당하는 유저의 정보를 가져왔음
-            if(passwordEncoder.matches(updatePwdRequest.getLastPassword(), member.getPassword())==false)
+            if(passwordEncoder.matches(updatePwdRequest.getLastPassword(), user.getPassword())==false)
                 return new ResponseTemplate<>(ResponseTemplateStatus.INVALID_PASSWORD);
             //유효성 검사
             if (bindingResult.hasErrors()) return new ResponseTemplate<>(ResponseTemplateStatus.INCORRECT_PASSWORD);
-
             userServiceImpl.updateUserPwd(email, updatePwdRequest); // 동일하다는 내용을 확인 됐다면 업데이트 진행
             String result = "회원 비밀번호 수정에 성공했습니다.";
-
             return new ResponseTemplate<>(result);
 
         } catch(ResponeException e){
